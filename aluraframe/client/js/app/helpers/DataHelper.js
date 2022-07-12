f58c7ed89ec7341c065ca40755f9135304f3838d
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-07-12 20:40:27 -0300

    Atualizando 'RegExp' do 'DataHelper.js'

--- aluraframe/client/js/app/helpers/DataHelper.js
+++ aluraframe/client/js/app/helpers/DataHelper.js
@@ -5,7 +5,7 @@ class DataHelper {
     }
 
     static textoEmData(texto) {
-        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error('Formato Inválido! A data deve ser aaaa-mm-dd')
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto)) throw new Error('Formato Inválido! A data deve ser aaaa-mm-dd')
         return new Date(...texto.split('-').map((valor, i) => valor - i % 2))
     }
 
